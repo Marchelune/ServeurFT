3227125e23aeb962d26c06c628b16fa938c98ea6
Rémi Sormain <remi.sormain@gmail.com>
2014-01-31 00:09:39 +0100

    Suppression de la classe Sérialiseur (ne fonctionnait qu'en local) remplacée par Serialiseur (fonctionne sur les serveurs GAE) Modification en conséquence de la servlet InfoUser

--- src/serveurFT1/InfoUser.java
+++ src/serveurFT1/InfoUser.java
@@ -10,22 +10,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import donnees.Serialiseur;
 
 
-import donnees.Sérialiseur;
+
 
 @SuppressWarnings("serial")
 public class InfoUser extends HttpServlet {
 
 	public void doGet( HttpServletRequest request, HttpServletResponse reponse ) throws ServletException, IOException{
 		reponse.setContentType("text/xml");
-		//long id = Integer.parseInt(request.getParameter( "id" ));
 		
 		PrintWriter out = reponse.getWriter();
+		Serialiseur serialiseur = new Serialiseur();
+
+		out.print(serialiseur.serialise());
 		
-		Sérialiseur sérialiseur = new Sérialiseur();
-		
-		out.print(sérialiseur.sérialiser());
 	}
 	
 	
